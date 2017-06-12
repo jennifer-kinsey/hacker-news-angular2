72d0b0f8e4ed70bbcdbd2596bcc1d759cdae0ad8
James Higgins and Jennifer Kinsey <jennifer@>
2017-06-12 10:57:54 -0700

    display title and author on homepage, and create story model

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Story } from '../story.model';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  stories: Story[] = [
+    new Story('Salaries of 100+ Software Engineers, Including Google, Amazon, Grand Rounds',
+     'https://docs.google.com/spreadsheets/d/1JR4KrVH1dygniLiLFAMTvdSp5gGLVKKkOxBYiPQagvE/edit#gid=0',
+     'tmeyster',
+     'tech',
+      1 ),
+    new Story('	Uber CEO Kalanick likely to take leave',
+     'http://www.reuters.com/article/us-uber-board-vote-idUSKBN1930AA',
+     'pdelbarba', 'tech', 2),
+    new Story('Weight Training Is the Best Exercise for Bone Strength',
+    'http://time.com/4803697/bodybuilding-strength-training/?utm_source=pocket&utm_medium=email&utm_campaign=pockethits',
+     'deegles', 'health', 3),
+    new Story('#c0ffee is the color', 'http://c0ffee.surge.sh/', 'pavel_lishin', 'design', 4)
+  ];
+
+}
---
+++ src/app/story.model.ts
@@ -0,0 +1,10 @@
+export class Story {
+   public rank: 0;
+  constructor (
+    public title: string,
+    public url: string,
+    public username: string,
+    public category: string,
+    public id: number
+  ) {}
+}
